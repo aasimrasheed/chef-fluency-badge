72ee61baff38b6c789d97c5ba228a9f599e3c906
Aasim Rasheed <aasim.rasheed@gmail.com>
2017-09-20 18:01:51 +0000

    Minor updates to Apache website.rb

--- cookbooks/apache/recipes/website.rb
+++ cookbooks/apache/recipes/website.rb
@@ -2,5 +2,5 @@
 
 file 'default www' do
 	path '/var/www/html/index.html'
-	content 'HhhhhhEeeeeeLllllLllllOooooo WwwwwOoooRrrrLlllDdddd'
+	content 'HhhhhhEeeeeeLllllLllllOooooo WwwwwOoooRrrrLlllDdddd --- version 0.1.1'
 end
